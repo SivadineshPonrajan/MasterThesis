b99857524b5a5392e9ad122148f7ca7aaae2f1df
Sivadinesh Ponrajan <sivadinesh107@gmail.com>
2024-05-13 11:38:02 +0200

    Added cuda support for YoloV3 detector

--- detector/scripts/detector.py
+++ detector/scripts/detector.py
@@ -1,8 +1,9 @@
 import os
+import json
 import rospy
 from message_filters import Subscriber, ApproximateTimeSynchronizer
 from sensor_msgs.msg import Image
-from image_publisher.msg import fname
+from geometry_msgs.msg import PoseStamped
 import cv2
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
@@ -20,6 +21,8 @@ class detector:
         self.classes_path = os.path.join(pwd_path, 'yolov3.txt')
 
         self.net = cv2.dnn.readNet(self.weights_path, self.config_path)
+        self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+        self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
         # In case of adding other dynamic objects
         # self.classes = []
@@ -27,21 +30,27 @@ class detector:
         #     self.classes = [line.strip() for line in f.readlines()]
         
         # Create subscribers
-        self.image_sub = Subscriber('image', Image)
-        self.filename_sub = Subscriber('filename', fname)
+        self.image_sub = Subscriber('/usb_cam/image_raw', Image)
+        self.pose_sub = Subscriber('/orb_slam3/camera_pose', PoseStamped)
         
         # Set up the ApproximateTimeSynchronizer
-        self.ts = ApproximateTimeSynchronizer([self.image_sub, self.filename_sub], 10, 0.1)
+        self.ts = ApproximateTimeSynchronizer([self.image_sub, self.pose_sub], 10, 0.1)
         self.ts.registerCallback(self.callback)
         
         # Folder to save images
-        self.folder_path = os.path.expanduser('~/Desktop/saved_images')
-        if not os.path.exists(self.folder_path):
-            os.makedirs(self.folder_path)
+        self.folder_path = os.path.expanduser('~/root/saved_images')
+        '''if not os.path.exists(self.folder_path):
+            os.makedirs(self.folder_path)'''
+
+        # Json file to save poses
+        self.json_path = os.path.join(self.folder_path, "image_pose.json")
+
+        self.image_pose_data = []
+        self.image_counter = 0
 
         # Publishers
-        self.image_pub = rospy.Publisher('/usb_cam/image_raw', Image, queue_size=10)
-        self.filename_pub = rospy.Publisher('/orb_slam3/camera_pose', fname, queue_size=10)
+        self.image_pub = rospy.Publisher('/detector/image_raw', Image, queue_size=10)
+        self.pose_pub = rospy.Publisher('/detector/camera_pose', PoseStamped, queue_size=10)
 
 
     def get_output_layers(self, net):
@@ -52,7 +61,7 @@ class detector:
             output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
         return output_layers
 
-    def callback(self, image_msg, filename_msg):
+    def callback(self, image_msg, pose_msg):
         try:
             # Convert the image to an OpenCV format
             cv_image = self.bridge.imgmsg_to_cv2(image_msg, "bgr8")
@@ -73,33 +82,42 @@ class detector:
                     confidence = scores[class_id]
                     if class_id == 0 and confidence > conf_threshold:
                         dynamic_object = True
-            
-            if dynamic_object:
-                print("Dynamic objects detected in image: ", {filename_msg.filename})
+
+            self.image_counter += 1
+            if not dynamic_object:
+                self.publish_results(image_msg, pose_msg)
             else:
-                print("Dynamic objects not detected in image: ", {filename_msg.filename})
-                self.publish_results(cv_image, filename_msg)
+                rospy.loginfo('Dynamic object detected in image_{}.png'.format(self.image_counter))
 
         except CvBridgeError as e:
             rospy.logerr(e)
             return
 
-    def publish_results(self, cv_image, filename_msg):
+    def publish_results(self, image_msg, pose_msg):
         try:
+            filename = 'image_{}.png'.format(self.image_counter)
             # Construct the full file path
-            full_path = os.path.join(self.folder_path, filename_msg.filename)
+            full_path = os.path.join(self.folder_path, filename)
             # Save the image to the desktop
-            cv2.imwrite(full_path, cv_image)
+            '''cv2.imwrite(full_path, cv_image)'''
             rospy.loginfo('Saved image to {}'.format(full_path))
 
+            '''# To Json file
+            datum = {
+                    "file": filename,
+                    "pose": str(pose_msg)
+                }
+            self.image_pose_data.append(datum)
+            with open(self.json_path, 'w') as json_file:
+                json.dump(self.image_pose_data, json_file, indent=4)'''
+
             # publish
-            image_msg_out = self.bridge.cv2_to_imgmsg(cv_image, "bgr8")
-            image_msg_out.header.stamp = rospy.Time.now()
-            self.image_pub.publish(image_msg_out)
-            filename_msg_out = fname()
-            filename_msg_out.filename = filename_msg.filename
-            filename_msg_out.header.stamp = image_msg_out.header.stamp
-            self.filename_pub.publish(filename_msg_out)
+            #image_msg_out = self.bridge.cv2_to_imgmsg(cv_image, "bgr8")
+            #image_msg_out.header.stamp = pose_msg.header.stamp
+            #self.image_pub.publish(image_msg_out)
+            self.image_pub.publish(image_msg)
+            
+            self.pose_pub.publish(pose_msg)
         except CvBridgeError as e:
             rospy.logerr("CvBridge Error {0}".format(e))
 
